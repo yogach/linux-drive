62b01a0fda685e6101cf558cc9b5ac781e54d1fc
yogach2 <471534369@qq.com>
2019-08-21 22:39:37 +0800

    增加注释

--- chrdev/Button/input_event/buttondrv.c
+++ chrdev/Button/input_event/buttondrv.c
@@ -1,6 +1,5 @@
 #include <linux/module.h>
 #include <linux/version.h>
-
 #include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/interrupt.h>
@@ -13,10 +12,28 @@
 #include <linux/platform_device.h>
 #include <linux/input.h>
 #include <linux/irq.h>
-
 #include <asm/gpio.h>
 #include <asm/io.h>
 #include <asm/arch/regs-gpio.h>
+
+
+/*
+测试方法：
+如果没有启动QT：
+cat /dev/tty1
+按:s2,s3,s4
+就可以得到ls
+
+
+或者：
+exec 0</dev/tty1  把标准输入改成/dev/tty1 等待tty1输入数据
+然后可以使用按键来输入
+
+
+
+*/
+
+
 
 static struct timer_list buttons_timer; //初始化定时器
 static struct input_dev* buttons_dev;
@@ -32,10 +49,10 @@ typedef struct pin_desc
 
 static T_pin_desc g_pindesc[4]=
 {
-	{IRQ_EINT0,"button1",S3C2410_GPF0,KEY_L},
-	{IRQ_EINT2,"button2",S3C2410_GPF2,KEY_S},
-	{IRQ_EINT11,"button3",S3C2410_GPG3,KEY_ENTER},
-	{IRQ_EINT19,"button4",S3C2410_GPG11,KEY_LEFTSHIFT},
+	{IRQ_EINT0,"S1",S3C2410_GPF0,KEY_L},
+	{IRQ_EINT2,"S2",S3C2410_GPF2,KEY_S},
+	{IRQ_EINT11,"S3",S3C2410_GPG3,KEY_ENTER},
+	{IRQ_EINT19,"S4",S3C2410_GPG11,KEY_LEFTSHIFT},
 }
 
 static struct pin_desc* irq_pd;
@@ -88,7 +105,7 @@ static int buttons_init ( void )
 	/* 2. 设置 */
 	/* 2.1 能产生哪类事件 include\linux\input.h定义*/
 	set_bit ( EV_KEY, buttons_dev->evbit );
-	set_bit ( EV_REP, buttons_dev->evbit );
+	set_bit ( EV_REP, buttons_dev->evbit );//设置能够产生重复事件 效果就是按下按键后会上报键值
 
 	/* 2.2 能产生这类操作里的哪些事件: L,S,ENTER,LEFTSHIT */
 	set_bit ( KEY_L, buttons_dev->keybit );
