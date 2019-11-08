7e6b93a8ba45d128ce5a0025d03b475e5a7e3a1f
caoyukui <caoyukui@viewshine.cn>
2019-11-08 15:31:00 +0800

    i2c例程直接使用i2c_new_device()函数新建设备

--- i2c/1th_i2c_new_device/at24cxx_dev.c
+++ i2c/1th_i2c_new_device/at24cxx_dev.c
@@ -6,14 +6,21 @@
 #include <linux/regmap.h>
 #include <linux/slab.h>
 
-
+//0x50为设备地址  
 static struct i2c_board_info at24cxx_info = {	
 	I2C_BOARD_INFO("at24c08", 0x50),
 };
 
+struct i2c_client *at24cxx_client;
 
 static int at24cxx_dev_init(void)
 {
+	struct i2c_adapter *i2c_adap;
+	
+	//驱动适配器 参数为总线编号 如果板子只有一个I2C控制器就填0
+	i2c_adap = i2c_get_adapter(0); 
+	at24cxx_client = i2c_new_device(i2c_adap, &at24cxx_info);
+	i2c_put_adapter(i2c_adap);
 
  
 
@@ -22,8 +29,7 @@ static int at24cxx_dev_init(void)
 
 static void at24cxx_dev_exit(void)
 {
-
-
+   i2c_unregister_device(at24cxx_client);
 
 }
 
