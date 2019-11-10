40f3a3c042412016bda08650d626145e61631bbd
yogach2 <471534369@qq.com>
2019-11-10 20:33:52 +0800

    用户空间直接使用i2c设备

---
+++ i2c/5th_user_space_test/i2c_usr_test.c
@@ -0,0 +1,79 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "i2c-dev.h"//使用当前目录下的i2c-dev设备
+
+
+/* i2c_usr_test </dev/i2c-0> <dev_addr> r addr
+ * i2c_usr_test </dev/i2c-0> <dev_addr> w addr val
+ */
+
+void print_usage ( char* file )
+{
+	printf ( "%s </dev/i2c-0> <dev_addr> r addr\n", file );
+	printf ( "%s </dev/i2c-0> <dev_addr> w addr val\n", file );
+}
+
+int main ( int argc, char** argv )
+{
+	int fd;
+	unsigned char addr,data;
+	int dev_addr;
+
+
+	if ( ( argc != 5 ) && ( argc != 6 ) )
+	{
+		print_usage ( argv[0] );
+		return -1;
+	}
+
+	fd = open ( argv[1], O_RDWR );
+	if ( fd < 0 )
+	{
+		printf ( "can't open %s\n",argv[1] );
+		return -1;
+	}
+
+	dev_addr = strtoul ( argv[2],NULL,0 ); //
+
+	//设置设备地址
+	if ( ioctl ( fd, I2C_SLAVE, dev_addr ) < 0 )
+	{
+		/* ERROR HANDLING; you can check errno to see what went wrong */
+		printf ( "set device address error\r\n" );
+		return -1;
+	}
+
+	if ( strcmp ( argv[4], "r" ) == 0 )
+	{
+	    //读操作
+		addr = strtoul ( argv[5], NULL, 0 );
+		data = i2c_smbus_read_word_data(fd, addr);
+
+		//分别读出数据的字符、整形和16进制数
+		printf ( "data: %c, %d, 0x%2x\n", data, data, data );
+	}
+	else if ( ( strcmp ( argv[4], "w" ) == 0 ) && ( argc == 6 ) )
+	{
+		addr = strtoul ( argv[5], NULL, 0 );
+		data = strtoul ( argv[6], NULL, 0 );
+
+		//写一个字节数据
+		i2c_smbus_write_byte_data(fd, addr, data);		
+
+		printf ( "write err, addr = 0x%02x, data = 0x%02x\n", addr, data );
+
+	}
+	else
+	{
+		print_usage ( argv[0] );
+		return -1;
+	}
+
+	return 0;
+}
+
