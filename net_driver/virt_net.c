4ae703435bdd26ff2e88e5a65c6029987bb064cb
yogach2 <471534369@qq.com>
2020-01-14 21:19:15 +0800

    新建虚拟网卡驱动

---
+++ net_driver/virt_net.c
@@ -0,0 +1,45 @@
+/*
+ * ²Î¿¼ drivers\net\cs89x0.c
+ */
+
+#include <linux/module.h>
+#include <linux/errno.h>
+#include <linux/netdevice.h>
+#include <linux/etherdevice.h>
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/fcntl.h>
+#include <linux/interrupt.h>
+#include <linux/ioport.h>
+#include <linux/in.h>
+#include <linux/skbuff.h>
+#include <linux/slab.h>
+#include <linux/spinlock.h>
+#include <linux/string.h>
+#include <linux/init.h>
+#include <linux/bitops.h>
+#include <linux/delay.h>
+#include <linux/ip.h>
+
+#include <asm/system.h>
+#include <asm/io.h>
+#include <asm/irq.h>
+
+
+
+static int virt_net_init(void)
+{
+   return 0;
+
+}
+
+sttaic void virt_net_exit(void)
+{
+
+}
+
+module_init(virt_net_init);
+module_exit(virt_net_exit);
+MODULE_LICENSE("GPL");
+
+
